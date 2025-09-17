9b3a2860483e3d4521c6fbd829df6e69e317a5d2
Samarth Narang <snarang@umass.edu>
2025-09-17 11:54:18 +0530

    Can get tripCount and loads/stores from AffineForOp

--- mlir/lib/Transforms/AffineCostModel.cpp
+++ mlir/lib/Transforms/AffineCostModel.cpp
@@ -1,6 +1,7 @@
 
 #include "mlir/Transforms/AffineCostModel.h"
 #include "mlir/Dialect/Affine/Analysis/AffineAnalysis.h"
+#include "mlir/Dialect/Affine/Analysis/LoopAnalysis.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Builders.h"
@@ -17,6 +18,26 @@ struct AffineCostModelPass
       llvm::errs() << "AffineForOp found at ";
       forOp.getLoc().print(llvm::errs());
       llvm::errs() << "\n";
+
+      // If there is a trip count, let's get that.
+      // getConstantTripCount takes a forOp as its argument.
+      auto tripCount = getConstantTripCount(forOp);
+      if (!tripCount.has_value())
+        llvm::errs() << "  Trip count: unknown\n";
+      else
+        llvm::errs() << "  Trip count: " << tripCount.value() << "\n";
+
+      // Count memory ops, i.e. loads and stores.
+      unsigned numLoads = 0, numStores = 0;
+      forOp->walk([&](Operation *op) {
+        if (isa<affine::AffineLoadOp>(op))
+          ++numLoads;
+        else if (isa<affine::AffineStoreOp>(op))
+          ++numStores;
+      });
+      llvm::errs() << "  Memory ops: " << (numLoads + numStores)
+                   << " (loads: " << numLoads << ", stores: " << numStores
+                   << ")\n";
     });
   }
 
